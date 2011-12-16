1e571a6d380d49c7608b1d076aac8e382d694490
Paul Shannon <paul@paulshannon.com>
2011-12-16 11:39:29 +0000

    added web project and hooked up the first message publisher

---
+++ src/SevenDigital.Content.Ingestion.Spike.Domain/Queuing/FakeBus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using SevenDigital.Content.Ingestion.Spike.Domain.Boilerplate;
+
+namespace SevenDigital.Content.Ingestion.Spike.Domain.Queuing
+{
+    public class FakeBus : ICommandSender, IEventPublisher
+    {
+        private readonly Dictionary<Type, List<Action<IMessage>>> _routes = new Dictionary<Type, List<Action<IMessage>>>();
+
+        public void RegisterHandler<T>(Action<T> handler) where T : IMessage
+        {
+            List<Action<IMessage>> handlers;
+            if (!_routes.TryGetValue(typeof(T), out handlers))
+            {
+                handlers = new List<Action<IMessage>>();
+                _routes.Add(typeof(T), handlers);
+            }
+            handlers.Add(DelegateAdjuster.CastArgument<IMessage, T>(x => handler(x)));
+        }
+
+        public void Send<T>(T command) where T : IMessage
+        {
+            List<Action<IMessage>> handlers;
+            if (_routes.TryGetValue(typeof(T), out handlers))
+            {
+                if (handlers.Count != 1) throw new InvalidOperationException("cannot send to more than one handler");
+                handlers[0](command);
+            }
+            else
+            {
+                throw new InvalidOperationException("no handler registered");
+            }
+        }
+
+        public void Publish<T>(T @event) where T : IMessage
+        {
+            List<Action<IMessage>> handlers;
+            if (!_routes.TryGetValue(@event.GetType(), out handlers)) return;
+            foreach (var handler in handlers)
+            {
+                //dispatch on thread pool for added awesomeness
+                var handler1 = handler;
+                ThreadPool.QueueUserWorkItem(x => handler1(@event));
+            }
+        }
+    }
+
+    public interface ICommandSender
+    {
+        void Send<T>(T command) where T : IMessage;
+
+    }
+    public interface IEventPublisher
+    {
+        void Publish<T>(T @event) where T : IMessage;
+    }
+}
---
+++ src/SevenDigital.Content.Ingestion.Spike.Web/FileDepositModule.cs
@@ -0,0 +1,31 @@
+﻿using Nancy;
+using SevenDigital.Content.Ingestion.Spike.Domain.Commands;
+using SevenDigital.Content.Ingestion.Spike.Domain.Handlers;
+using SevenDigital.Content.Ingestion.Spike.Domain.Queuing;
+
+namespace SevenDigital.Content.Ingestion.Spike.Web
+{
+    public class FileDepositModule : NancyModule
+    {
+        public FileDepositModule(ICommandSender sender): base("deposit")
+        {
+            Get["/"] = _ =>
+                           {
+                               sender.Send(new FileDeposit());
+                               return "File Deposit Made";
+                           };
+        }
+    }
+
+    public class FakeBusBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoC.TinyIoCContainer container)
+        {
+            var bus = new FakeBus();
+            var depositDetector = new DepositDetector();
+            bus.RegisterHandler<FileDeposit>(depositDetector.Handle);
+            container.Register<ICommandSender, FakeBus>(bus).AsSingleton();
+            base.ConfigureApplicationContainer(container);
+        }
+    }
+}
